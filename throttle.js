672798ad61e6e4164b94c319ee98c241e5405d4d
Suman Gurung <92732976+SumanGurung01@users.noreply.github.com>
2024-02-29 15:45:33 +0530

    add: add throttle code

---
+++ throttle.js
@@ -0,0 +1,27 @@
+const input = document.querySelector(".input");
+const paragraph = document.querySelector(".paragraph");
+
+// debounce code
+function throttle(callback, delay) {
+    var wait = false;
+
+    return (e) => {
+        if (wait) return;
+
+        callback(e);
+
+        wait = true;
+
+        setTimeout(() => {
+            wait = false;
+        }, delay);
+    };
+}
+
+function handleChange(e) {
+    paragraph.innerText = e.target.value;
+}
+
+const throttleFn = throttle(handleChange, 1000);
+
+input.addEventListener("input", throttleFn);
